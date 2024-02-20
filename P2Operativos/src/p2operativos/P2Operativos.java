a7ab8d23dc8a1afdc5264e21b2237ee4b0fcb670
isabellacarrillo <isabella.carrillo@correo.unimet.edu.ve>
2024-02-20 13:57:14 -0400

    first commit

---
+++ P2Operativos/src/p2operativos/P2Operativos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p2operativos;
+
+/**
+ *
+ * @author Admin
+ */
+public class P2Operativos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
