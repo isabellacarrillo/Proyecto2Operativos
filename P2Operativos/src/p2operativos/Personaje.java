8a008f1efd5f883f286b210665da3973b9528125
isabellacarrillo <isabella.carrillo@correo.unimet.edu.ve>
2024-02-20 18:10:24 -0400

    clases empezadas

---
+++ P2Operativos/src/p2operativos/Personaje.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p2operativos;
+
+/**
+ *
+ * @author Admin
+ */
+public class Personaje {
+    
+    private Personaje next;
+
+    /**
+     * @return the next
+     */
+    public Personaje getNext() {
+        return next;
+    }
+
+    /**
+     * @param next the next to set
+     */
+    public void setNext(Personaje next) {
+        this.next = next;
+    }
+    
+    
+}
---
+++ P2Operativos/src/p2operativos/Queue.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package p2operativos;
+
+/**
+ *
+ * @author Admin
+ */
+public class Queue {
+    
+      private Personaje head; 
+    private Personaje tail; 
+    public int size; 
+    
+    public Queue(){
+        this.head = null; 
+        this.tail = null; 
+        this.size = 0; 
+    }
+    
+    public boolean isEmpty(){
+        return this.head == null;
+    }
+    
+    public void empty(){
+        this.head = this.tail = null;
+        this.size = 0;
+    }
+    
+    public void enqueue(Personaje nuevo){
+        if(this.isEmpty()){
+            head = tail = nuevo;
+        }else{
+            tail.setNext(nuevo);
+            tail = nuevo;
+        }
+        size++;
+    }
+    
+    public void dequeue(){
+        if(this.isEmpty()){
+        }else if(size == 1){
+            
+            this.empty();
+        }else{
+            head = head.getNext();
+            size--;
+        }
+    }
+    
+    public Personaje getHead() {
+        return head;
+    }
+
+    public void setHead(Personaje head) {
+        this.head = head;
+    }
+
+    public Personaje getTail() {
+        return tail;
+    }
+
+    public void setTail(Personaje tail) {
+        this.tail = tail;
+    }
+    
+}
